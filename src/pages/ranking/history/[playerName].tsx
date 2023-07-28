f62af44484b63fc78097d6f6f6779aa58408af3c
Federico Viotti <fedeviotti@gmail.com>
2023-07-28 14:58:52 +0200

    fix: player games add skeleton and bg box

--- src/pages/ranking/history/[playerName].tsx
+++ src/pages/ranking/history/[playerName].tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Head from "next/head";
 import {
+  Box,
   Button,
   Heading, Skeleton, Stack, Text,
 } from "@chakra-ui/react";
@@ -30,6 +31,8 @@ const History = () => {
           <Skeleton height="60px" />
           <Skeleton height="60px" />
           <Skeleton height="60px" />
+          <Skeleton height="60px" />
+          <Skeleton height="60px" />
         </>
       );
     }
@@ -38,7 +41,7 @@ const History = () => {
         id, player, opponent, delta, winner,
       }) => (
         <Stack key={id}>
-          <Text fontSize="2xl">
+          <Text fontSize="2xl" px={4}>
             {`vs. ${player === playerName ? opponent : player}: +${calculateGameScore(delta || 0, winner || "")} pts.`}
           </Text>
           <MovesList gameId={id} />
@@ -57,9 +60,13 @@ const History = () => {
       <Stack spacing={16}>
         <Button onClick={back} alignSelf="center">Back to ranking</Button>
         <Heading alignSelf="center">{`${playerName}'s History`}</Heading>
-        <Stack spacing={12}>
-          {getContent()}
-        </Stack>
+        <Box>
+          <Box borderWidth="1px" borderRadius="lg" alignItems="center" bg="gray.50" overflow="auto">
+            <Stack spacing={12}>
+              {getContent()}
+            </Stack>
+          </Box>
+        </Box>
       </Stack>
     </>
   );
