1a65dce5b5c2b43b87e681f697e69ec44081303a
Federico Viotti <fedeviotti@gmail.com>
2023-08-07 14:59:01 +0200

    refactor(ranking): edit calculateTotalPlayerScore function

--- src/features/ranking/utils/calculateTotalPlayerScore.ts
+++ src/features/ranking/utils/calculateTotalPlayerScore.ts
@@ -1,9 +1,7 @@
 type Props = {
   delta: number;
-  games: number | undefined;
 };
 
 export const calculateTotalPlayerScore = ({
   delta = 0,
-  games = 0,
-}: Props) => (delta * 10) + ((games) * 10);
+}: Props) => (delta * 10);
