dfa7bb09d5fb3dba4b01a9eabcca1401bd6227c4
Federico Viotti <fedeviotti@gmail.com>
2024-01-16 00:14:36 +0100

    fix(footer): add space before author name

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,9 +2,8 @@ import { Flex } from "@chakra-ui/react";
 
 const Footer = () => (
   <Flex w="100%" p={8} justifyContent="center" color="white">
-    Made with ❤️ by
-    {" "}
-    <a href="https://github.com/fedeviotti">@fedeviotti</a>
+    Made with  ❤️ by
+    <a href="https://github.com/fedeviotti" style={{ marginLeft: "3px" }}>@fedeviotti</a>
   </Flex>
 );
 
